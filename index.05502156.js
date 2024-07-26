2024138e9c1e31ad6d48eb8017081292561fb12a
Valentyn Radobenko <valikradobenko@gmail.com>
2024-07-26 19:29:53 +0300

    make build

---
+++ index.05502156.js
@@ -0,0 +1,2 @@
+for(var t=function(t){r[t].addEventListener("click",function(){r[t].setAttribute("href","#"),e.setAttribute("src","images/"+l[t].getAttribute("src").split("-thumb")[0]+".png")})},e=document.querySelector("#largeImg"),r=document.querySelectorAll(".list-item__link"),l=document.querySelectorAll(".gallery__thumb"),u=0;u<r.length;u++)t(u);
+//# sourceMappingURL=index.05502156.js.map
